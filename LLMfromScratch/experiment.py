34eceab2e5d5c72036b145078420197162557a65
Srijanagits <srijanashrestha013@gmail.com>
2025-10-13 22:16:14 -0400

    updated experiment.py

---
+++ LLMfromScratch/experiment.py
@@ -0,0 +1,126 @@
+import torch 
+import os
+import sys
+import tiktoken
+from architecture import GPTModel
+from train import train_model_simple
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from TextDataProcessing.data import GPTDatasetV1, create_dataloader_v1
+
+
+# ---------------- Load book text ----------------
+txt_path = "BookAndDataFiles/book.txt"  # relative path to your project root
+
+try:
+    with open(txt_path, "r", encoding="utf-8") as f:
+        text_data = f.read()
+    print("text_data loaded successfully!")
+    print("Length of text_data:", len(text_data))
+    print("First 300 characters:\n", text_data[:300])
+except FileNotFoundError:
+    print(f"File not found: {txt_path}. Make sure DataPipeline.ipynb has been run.")
+    exit()
+
+
+def main(gpt_config, settings):
+
+    torch.manual_seed(123)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+    ##############################
+    # Initialize model
+    ##############################
+
+    model = GPTModel(gpt_config)
+    model.to(device)  # no assignment model = model.to(device) necessary for nn.Module classes
+    optimizer = torch.optim.AdamW(
+        model.parameters(), lr=settings["learning_rate"], weight_decay=settings["weight_decay"]
+    )
+
+    ##############################
+    # Set up dataloaders
+    ##############################
+
+    # Train/validation ratio
+    train_ratio = 0.90
+    split_idx = int(train_ratio * len(text_data))
+
+    train_loader = create_dataloader_v1(
+        text_data[:split_idx],
+        batch_size=settings["batch_size"],
+        max_length=gpt_config["context_length"],
+        stride=gpt_config["context_length"],
+        drop_last=True,
+        shuffle=True,
+        num_workers=0
+    )
+
+    val_loader = create_dataloader_v1(
+        text_data[split_idx:],
+        batch_size=settings["batch_size"],
+        max_length=gpt_config["context_length"],
+        stride=gpt_config["context_length"],
+        drop_last=False,
+        shuffle=False,
+        num_workers=0
+    )
+
+    ##############################
+    # Train model
+    ##############################
+
+    tokenizer = tiktoken.get_encoding("gpt2")
+
+    train_losses, val_losses, tokens_seen = train_model_simple(
+        model, train_loader, val_loader, optimizer, device,
+        num_epochs=settings["num_epochs"], eval_freq=5, eval_iter=1,
+        start_context="Every effort moves you", tokenizer=tokenizer
+    )
+
+    return train_losses, val_losses, tokens_seen, model
+
+
+if __name__ == "__main__":
+
+    GPT_CONFIG_124M = {
+        "vocab_size": 50257,    # Vocabulary size
+        "context_length": 256,  # Shortened context length (orig: 1024)
+        "emb_dim": 768,         # Embedding dimension
+        "n_heads": 12,          # Number of attention heads
+        "n_layers": 12,         # Number of layers
+        "drop_rate": 0.1,       # Dropout rate
+        "qkv_bias": False       # Query-key-value bias
+    }
+
+    OTHER_SETTINGS = {
+        "learning_rate": 5e-4,
+        "num_epochs": 10,
+        "batch_size": 2,
+        "weight_decay": 0.1
+    }
+
+    ###########################
+    # Initiate training
+    ###########################
+
+    train_losses, val_losses, tokens_seen, model = main(GPT_CONFIG_124M, OTHER_SETTINGS)
+
+    ###########################
+    # After training
+    ###########################
+
+    # Plot results
+    epochs_tensor = torch.linspace(0, OTHER_SETTINGS["num_epochs"], len(train_losses))
+    plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+    plt.savefig("loss.pdf")
+
+    # Save and load model
+    torch.save(model.state_dict(), "model.pth")
+    model = GPTModel(GPT_CONFIG_124M)
+    model.load_state_dict(torch.load("model.pth", weights_only=True))
+
+
+    
